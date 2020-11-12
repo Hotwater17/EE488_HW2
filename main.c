b2fa29fe64321e45fc3fe645b16c6c79bf661d68
Hotwater17 <michgor@o2.pl>
2020-11-12 21:01:08 +0100

    Implicit.

--- main.c
+++ main.c
@@ -6,15 +6,14 @@
 int main()
 {
 
- //void *heapStart = tc_central_init();
+ void *heapStart = tc_central_init();
  void *thread1 = tc_thread_init();
  void *ptr1 = tc_malloc(10);
  tc_free(ptr1);
- void *ptr2 = tc_malloc(10);
  ptr1 = tc_malloc(10);
- //void *ptr2 = tc_malloc(20);
- /*void *ptr3 = tc_malloc(30);
- void *ptr4 = tc_malloc(1000);
+ void *ptr2 = tc_malloc(20);
+ void *ptr3 = tc_malloc(30);
+ void *ptr4 = tc_malloc(100);
  void *fitPtr;
  header *ptrstruct = ptr1;
  printf("%p %p %p %ld %p \n", heapStart, thread1, ptr1, ptrstruct->size, fitPtr);
@@ -22,11 +21,11 @@ int main()
  printf(" FREEING ptr1 \n");
  fitPtr = find_fit(ALIGN(25)+SIZE_T_SIZE);
  printf("Freeing ptr2 and 4 \n");
-tc_free(ptr2);
+ tc_free(ptr2);
  tc_free(ptr4);
  fitPtr = find_fit(ALIGN(25)+SIZE_T_SIZE);
  printf("%p %ld %p \n", ptrstruct, ptrstruct->size, fitPtr);
-*/ 
+
 return 0;
 }
 
--- tc_malloc.c
+++ tc_malloc.c
@@ -19,7 +19,7 @@ void *tc_central_init()
 {
  /*Initialize central heap and return a pointer to first address of free list */
  //Look at mmap();
- /*node *head = mmap(NULL, ALIGN(HEAP_SIZE), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE,
+ node *head = mmap(NULL, ALIGN(HEAP_SIZE), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE,
 			-1,0);
 
  head->size = HEAP_SIZE-sizeof(node);
@@ -29,7 +29,7 @@ void *tc_central_init()
  heapEnd = heapStart + HEAP_SIZE;
  printf("Start: %p End: %p \n", heapStart, heapEnd);
  return head;
-*/
+/*
  free_block_head_t *bp = mmap(NULL, ALIGN(HEAP_SIZE),
 			PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1,0);
  heapStart = bp;
@@ -38,7 +38,7 @@ void *tc_central_init()
  bp->next = bp;
  bp->prev = bp;
  printf("start %p end %p \n", heapStart, heapEnd);
-
+*/
 }
 
 void *tc_thread_init()
@@ -58,13 +58,13 @@ void *tc_malloc(size_t size)
  /*Allocate an object of size and return a pointer to it*/
  size_t alignedSize = ALIGN(size + SIZE_T_SIZE);
  //header * head = mmap(NULL, alignedSize, PROT_READ|PROT_WRITE,
- //                                MAP_ANON|MAP_PRIVATE, -1, 0);
- /*header *head = find_fit(alignedSize);
+  //                               MAP_ANON|MAP_PRIVATE, -1, 0);
+ header *head = find_fit(alignedSize);
  if(head) head->size = alignedSize | 1;
  else printf("Not enough heap space \n");
  head->next = (void *)head+(head->size & ~1L);
- return head;*/
- static int isHeapInit = 0;
+ return head;
+ /*static int isHeapInit = 0;
  if(!isHeapInit)
  {
   isHeapInit = 1;
@@ -75,18 +75,18 @@ void *tc_malloc(size_t size)
   else
   {
    printf("Not enough heap space \n");
-   
   }
   bp->next = (void *)bp + bp->size;
   printf("head %p size %ld head+size %p next %p prev %p \n", bp, bp->size,
 	(void *)bp + bp->size, bp->next, bp->prev);
   return bp;
+*/
 }
 
 void tc_free(void *ptr)
 {
  /*deallocate *ptr object*/
-  /*header *head = ptr;
+  header *head = ptr;
   head->size = head->size & ~1L;
   //Coalesce
   printf("next %p \n", head->next);
@@ -95,7 +95,7 @@ void tc_free(void *ptr)
   {
    head->size += head->next->size;
   }
-*/
+/*
   free_block_head_t *bp = ptr;
   bp->size = bp->size & ~1L;
 
@@ -105,11 +105,12 @@ void tc_free(void *ptr)
  {
   bp->size += bp->next->size;
  }
+*/
 }
 
 void *find_fit(size_t fitSize)
 {
-/*
+
  header *head = heapStart;
  while((void *)head < (void *)heapEnd)
  {
@@ -126,7 +127,7 @@ head =  (void *)head + (head->size & ~1L);
  printf("Space not found \n");
  return NULL;
 
-*/
+/*
 
  free_block_head_t *bp = heapStart;
  while((void *)bp < (void *)heapEnd)
@@ -143,4 +144,5 @@ head =  (void *)head + (head->size & ~1L);
  }
  printf("fit not found \n");
  return NULL;
+*/
 }
